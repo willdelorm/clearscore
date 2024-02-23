7242715512c66499457b6e5f07fe562272d36a92
Will Delorm <willdelorm@gmail.com>
2024-02-23 09:53:49 -0800

    remove redundant import

--- src/__test__/App.test.tsx
+++ src/__test__/App.test.tsx
@@ -1,6 +1,5 @@
 import { describe, expect, test } from "vitest";
 import { render, screen } from "@testing-library/react";
-import "@testing-library/jest-dom";
 import App from "../App";
 
 describe("App tests", () => {
