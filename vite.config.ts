10fb6906d4372861ea619158e4f3748b427b3915
Will Delorm <willdelorm@gmail.com>
2025-02-03 20:31:11 -0800

    add gh-pages base url

--- vite.config.ts
+++ vite.config.ts
@@ -7,9 +7,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-    test: {
-      globals: true,
-      environment: 'jsdom',
-      setupFiles: "./vitest-setup.ts"
-    }
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: "./vitest-setup.ts"
+  },
+  base: '/clearscore',
 })
